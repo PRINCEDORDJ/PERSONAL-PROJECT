7be32a3ef214f761c1d6ace4752c92b45d8389fc
princedordj-lab <princedordj@gmail.com>
2025-11-03 11:35:29 +0000

    feat: removed gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    supabaseUrl: 'https://wwfshnuzwxpglemenyam.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3ZnNobnV6d3hwZ2xlbWVueWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTk3NzIsImV4cCI6MjA3NzczNTc3Mn0.BF_eKlVkZfMBUvNQKZiSg-44jiLu0yj6RJDX5ltq8F4',
+    emailjsServiceID: 'service_9h1noek',
+    emailjsTemplateID: 'template_409zndf',
+    emailjsPublicKey: 'q5jpQQaOkdcffFwnd'
+};
